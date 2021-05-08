f6741b5b15fdd65bd891a5c3209b8a2273585d3c
Rick Booth <admin@rickbooth.me>
2021-05-08 21:01:09 +0100

    feat: add images to discover experiences

--- src/components/DiscoverExperiences.tsx
+++ src/components/DiscoverExperiences.tsx
@@ -9,17 +9,17 @@ type Card = {
 const experiences: Card[] = [
     {
         subTitle: "Travel from home with Online Experiences.",
-        image: "https://placedog.net/200/150/b?random",
+        image: "https://source.unsplash.com/TXxiFuQLBKQ/450x450",
         title: "Featured collection: Wanderlust",
     },
     {
         subTitle: "Live, interactive activities led by Hosts.",
-        image: "https://placedog.net/200/150/b?random",
+        image: "https://source.unsplash.com/GNVxujZ_CxU/450x450",
         title: "Online Experiences",
     },
     {
         subTitle: "Local things to do, wherever you are.",
-        image: "https://placedog.net/200/150/b?random",
+        image: "https://source.unsplash.com/wc_bUQFR7qc/450x450",
         title: "Experiences",
     },
 ]
@@ -28,7 +28,7 @@ export const DiscoverExperiences: React.VFC = () => {
     return (
         <div className="discover-experiences mt-16 mb-16 mx-8 md:mx-16">
             <h2 className="font-bold text-3xl mb-2">Discover Experiences</h2>
-            <h3 className="text-2xl mb-4">
+            <h3 className="text-2xl mb-6">
                 Unique activities with local experts - in person or online.
             </h3>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -47,7 +47,10 @@ const DiscoverExperiencesCard: React.VFC<Card> = (props) => {
     const { subTitle, title, image } = props
     return (
         <a href="#" className="flex flex-col">
-            <img src={image} className="rounded-xl mb-2 h-full " />
+            <img
+                src={image}
+                className="rounded-xl mb-2 h-[300px] md:h-[450px] object-cover"
+            />
             <h4 className="bold text-xl mb-2">{title}</h4>
             <p>{subTitle}</p>
         </a>
