0b69706bf6f64e7fb8f2aedc0af826682a117336
Rick Booth <admin@rickbooth.me>
2021-05-08 21:18:45 +0100

    feat: add images to explore nearby

--- src/components/ExploreNearby.tsx
+++ src/components/ExploreNearby.tsx
@@ -1,16 +1,44 @@
 import React from "react"
 
-type Location = { name: string; duration: string }
+type Location = { name: string; duration: string; image: string }
 
 const locations: Location[] = [
-    { name: "Windermere", duration: "3-hour drive" },
-    { name: "Whitby", duration: "2-hour drive" },
-    { name: "Newquay", duration: "6.5-hour drive" },
-    { name: "York", duration: "1-hour drive" },
-    { name: "Blackpool", duration: "2-hour drive" },
-    { name: "Sheffield", duration: "45-minute drive" },
-    { name: "Harrogate", duration: "1-hour drive" },
-    { name: "Manchester", duration: "1.5-hour drive" },
+    {
+        name: "Windermere",
+        duration: "3-hour drive",
+        image: "../../public/windermere.jpg",
+    },
+    {
+        name: "Whitby",
+        duration: "2-hour drive",
+        image: "../../public/whitby.jpg",
+    },
+    {
+        name: "Newquay",
+        duration: "6.5-hour drive",
+        image: "../../public/newquay.jpg",
+    },
+    { name: "York", duration: "1-hour drive", image: "../../public/york.jpg" },
+    {
+        name: "Blackpool",
+        duration: "2-hour drive",
+        image: "../../public/blackpool.jpg",
+    },
+    {
+        name: "Sheffield",
+        duration: "45-minute drive",
+        image: "../../public/sheffield.jpg",
+    },
+    {
+        name: "Harrogate",
+        duration: "1-hour drive",
+        image: "../../public/harrogate.jpg",
+    },
+    {
+        name: "Manchester",
+        duration: "1.5-hour drive",
+        image: "../../public/manchester.jpg",
+    },
 ]
 
 export const ExploreNearby: React.VFC = () => {
@@ -19,11 +47,7 @@ export const ExploreNearby: React.VFC = () => {
             <h2 className="text-3xl pb-4 font-bold">Explore nearby</h2>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 {locations.map((location) => (
-                    <ExploreNearbyCard
-                        key={location.name}
-                        name={location.name}
-                        duration={location.duration}
-                    />
+                    <ExploreNearbyCard key={location.name} {...location} />
                 ))}
             </div>
         </div>
@@ -31,14 +55,11 @@ export const ExploreNearby: React.VFC = () => {
 }
 
 const ExploreNearbyCard: React.VFC<Location> = (props) => {
-    const { duration, name } = props
+    const { duration, name, image } = props
     return (
         <a href="#" className="flex">
             <div>
-                <img
-                    className="h-16 rounded-lg"
-                    src="https://via.placeholder.com/64"
-                />
+                <img className="h-16 rounded-lg" src={image} />
             </div>
             <div className="flex flex-col justify-center ml-4">
                 <p className="font-bold mb-1">{name}</p>
