f4a84eb8fd2ea69cb68cbed150ca2aacbb2d66d6
Rick Booth <admin@rickbooth.me>
2021-05-08 16:27:11 +0100

    feat: add airbnb colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
     purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                orange: "#c13515",
+                red: "#b32505",
+                yellow: "#e07912",
+                green: "#008a05",
+                blue: "#428bff",
+                pink: "#ff385c",
+                "dark-pink": "#92174d",
+                purple: "#460479",
+                grey: "#b0b0b0",
+                "dark-grey": "#717171",
+                "darkest-grey": "#222",
+            },
+        },
     },
     variants: {
         extend: {},
